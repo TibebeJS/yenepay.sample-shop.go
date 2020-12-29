040710b940e121072e6e9957b63729bcf372bf8f
TibebeJs <Tibebes.js@gmail.com>
2020-12-29 20:08:16 +0300

    improve card UI

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -29,6 +29,21 @@ func (c App) Index() revel.Result {
 			"Learn Go",
 			"/public/img/get-programming-with-go.jpg",
 		},
+		{
+			"Go",
+			"Learn Go",
+			"/public/img/go-bootcamp.jpg",
+		},
+		{
+			"Automate The Boring Stuff With Python",
+			"Learn Go",
+			"/public/img/automate-the-boring-stuff-with-python.jpg",
+		},
+		{
+			"Get Programming With Go",
+			"Learn Go",
+			"/public/img/get-programming-with-go.jpg",
+		},
 	}
 
 	return c.Render(Books)
