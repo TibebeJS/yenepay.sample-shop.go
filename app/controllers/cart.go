9c496a7e30960852523527fd44e4b7e9d7a975bc
TibebeJs <Tibebes.js@gmail.com>
2021-01-05 11:20:07 +0300

    implement "remove cart item"

--- app/controllers/cart.go
+++ app/controllers/cart.go
@@ -1,5 +1,34 @@
 package controllers
 
+import (
+	"strconv"
+
+	"github.com/TibebeJs/yenepay.sample-shop.go/app/models"
+	"github.com/revel/revel"
+)
+
 type Cart struct {
 	Application
+}
+
+func (c Cart) RemoveItem() revel.Result {
+	bookId, err := strconv.Atoi(c.Params.Query.Get("book_id"))
+
+	if err != nil {
+		return c.Redirect(Application.Index)
+	}
+
+	user := c.connected()
+	if user == nil {
+		c.Flash.Error("Please log in first")
+		return c.Redirect(Application.Index)
+	}
+
+	_, err = c.Txn.Delete(&models.CartItem{UserId: user.UserId, BookId: bookId})
+	if err != nil {
+		c.Flash.Error("Couldn't remove a cart item")
+		c.Log.Fatal("Unexpected error removing a cart item", "error", err)
+	}
+
+	return c.Redirect(Application.Index)
 }
